e6d0d2af2f0163160a1a843c4cfc867212a5667d
ToshanR <20250802675@dypiu.ac.in>
2026-03-24 21:03:32 +0530

    Refactor calculator code for improved structure
    Updated variable names and streamlined operator handling for clarity.

--- Calc.cpp
+++ Calc.cpp
@@ -1,40 +1,41 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-    int x,y;
+    int a, b;
     char op;
     cout<<"Enter first number: ";
-    cin>>x;
-    cout<<"Enter operator (Add[+], Subtract[-], Multiply[*], Division[/], and Modulo Division[%]): ";
+    cin>>a;
+    cout<<"Choose operator from [+, -, *, /, %]: ";
     cin>>op;
     cout<<"Enter second number: ";
-    cin>>y;
-    switch(op){
-        case '+':
-            cout<<"Sum: "<<x+y;
-            break;
-        case '-':
-            cout<<"Difference: "<<x-y;
-            break;
-        case '*':
-            cout<<"Product: "<<x*y;
-            break;
-        case '/':
-            if(y!=0){
-                cout<<"Quotient: "<<x/y;
-            }else{
-                cout<<"Error: Don't divide by zero. Try different numbers.";
-            }
-            break;
-        case '%':
-            if(y!=0){
-                cout<<"Remainder: "<<x%y;
-            }else{
-                cout<<"Error: Don't divide by zero. Try different numbers!";
-            }
-            break;
-        default:
-            cout<<"Invalid operator!";
+    cin>>b;
+    if(op == '+'){
+        cout<<"Sum = "<<a+b;
+    }
+    else if(op == '-'){
+        cout<<"Difference = "<<a-b;
+    }
+    else if(op == '*'){
+        cout<<"Product = "<<a*b;
+    }
+    else if(op == '/'){
+        if(b != 0){
+            cout<<"Quotient = "<<a/b;
+        }
+        else{
+            cout<<"Error!";
+        }
+    }
+    else if(op == '%'){
+        if(b != 0){
+            cout<<"Remainder = "<<a%b;
+        }
+        else{
+            cout<<"Error!";
+        }
+    }
+    else{
+        cout<<"Invalid operator!";
     }
     return 0;
 }
